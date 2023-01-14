f0c78ec32b57f9765e2606048464a3251c10eaa7
matthewhadley <matthew.hadley@gmail.com>
2023-01-14 15:37:10 -0600

    debug log file

--- sense/app.js
+++ sense/app.js
@@ -1,17 +1,50 @@
+import {createWriteStream} from 'fs'
+import Stream from 'stream';
 import consoleStamp from 'console-stamp';
-consoleStamp(console, {
-    format: `:date(yyyy-mm-dd HH:MM:ss) :label() :msg()`,
-    tokens: {
-        label: (t) => {
-            return `${t.method.toUpperCase()}`;
-        }
-    }
-} );
-
 import dayjs from 'dayjs';
 import WebSocket from 'ws';
 import fetch from 'node-fetch';
-import * as fs from 'node:fs/promises';
+import fs from 'fs/promises'
+
+// consoleStamp(console, {
+//     format: `:date(yyyy-mm-dd HH:MM:ss) :label() :msg()`,
+//     tokens: {
+//         label: (t) => {
+//             return `${t.method.toUpperCase()}`;
+//         }
+//     }
+// } );
+
+const LOG_FILE = "/config/sense.log"
+const log = createWriteStream(LOG_FILE, { flags: 'a' });
+const logger = new console.Console(log, log);
+
+const stdOutTransform = new Stream.Transform({
+    transform(chunk, encoding, callback) {
+        process.stdout.write(chunk);
+        log.write(chunk);
+        callback(null, chunk.toString());
+    }
+});
+
+const stdErrTransform = new Stream.Transform({
+    transform(chunk, encoding, callback) {
+        process.stderr.write(chunk);
+        log.write(chunk);
+        callback(null, chunk.toString());
+    }
+});
+
+consoleStamp(logger, {
+    stdout: stdOutTransform,
+    stderr: stdErrTransform,
+    format: `:date(yyyy-mm-dd HH:MM:ss) :label() :msg()`,
+      tokens: {
+          label: (t) => {
+              return `${t.method.toUpperCase()}`;
+          }
+      }
+});
 
 
 const CONFIG_FILE = "/data/sense.conf";
@@ -22,7 +55,7 @@ const SENSE_PASSWORD = process.env.SENSE_PASSWORD;
 const SENSE_INTERVAL = parseInt(process.env.SENSE_INTERVAL);
 const SUPERVISOR_TOKEN = process.env.SUPERVISOR_TOKEN;
 
-console.info(`Sense ${SENSE_VERSION}`);
+logger.info(`Sense ${SENSE_VERSION}`);
 
 const SENSE_API_URI="https://api.sense.com/apiservice/api/v1"
 const SENSE_WS_URI="wss://clientrt.sense.com/monitors"
@@ -36,12 +69,12 @@ async function auth(reauth) {
     }
 
     if (conf.access_token && !reauth) {
-        console.info('Using cached Access Token');
+        logger.info('Using cached Access Token');
         return conf;
     }
 
     if (conf.refresh_token) {
-        console.info('Requesting new Access Token via Refresh Token');
+        logger.info('Requesting new Access Token via Refresh Token');
 
         const conf = JSON.parse(await fs.readFile(CONFIG_FILE, 'utf8'));
 
@@ -52,7 +85,7 @@ async function auth(reauth) {
         const response = await fetch(`${SENSE_API_URI}/renew`, { method: 'POST', body: params });
         const data = await response.json();
 
-        console.info('Got Access Token');
+        logger.info('Got Access Token');
 
         conf.access_token = data.access_token;
         conf.refresh_token = data.refresh_token;
@@ -60,7 +93,7 @@ async function auth(reauth) {
         await fs.writeFile(CONFIG_FILE, JSON.stringify(conf));
 
     } else {
-        console.info('Requesting new Access Token');
+        logger.info('Requesting new Access Token');
 
         const params = new URLSearchParams();
         params.append('email', SENSE_EMAIL);
@@ -69,7 +102,7 @@ async function auth(reauth) {
         const response = await fetch(`${SENSE_API_URI}/authenticate`, {method: 'POST', body: params});
         const data = await response.json();
 
-        console.info('Got Access Token');
+        logger.info('Got Access Token');
 
         conf.access_token = data.access_token;
         conf.refresh_token = data.refresh_token;
@@ -88,27 +121,30 @@ const connect = async function (conf) {
     const ws = new WebSocket(URI)
 
     let i = 0
-    console.info(`Connecting to websocket... (message interval rate: ${SENSE_INTERVAL})`)
-    ws.on('close', function() {
-        console.warn('Connection Closed');
-        console.warn('Attempting to reconnect');
+    logger.info(`Connecting to websocket... (message interval rate: ${SENSE_INTERVAL})`)
+    ws.on('close', function(code, reason) {
+        logger.warn('Connection Closed');
+        logger.warn('Code:', code);
+        logger.warn('Reason:', reason.toString());
+        logger.warn('Attempting to reconnect');
         setTimeout(async function () {
             let conf = await auth(true);
             connect(conf);
         }, 1000);
     });
     ws.on('error', function(error) {
-        console.warn('Connection Error', error);
+        logger.warn('Connection Error', error);
     });
     ws.on('open', function open() {
         ws.on('message', async function message(data) {
             data = JSON.parse(data);
             let type = data.type;
+            logger.info(type);
             if (type == "hello") {
-                console.info("Connection established");
+                logger.info("Connection established");
             } else if (type == "error") {
                 const error = data.payload.error_reason;
-                console.error(error);
+                logger.error(error);
                 if (error === "Unauthorized") {
 
                 } else if (error === "Unavailable") {
@@ -116,7 +152,7 @@ const connect = async function (conf) {
                 }
             } else if (type === "realtime_update") {
                 if (i === SENSE_INTERVAL || i === 0) {
-                    // console.info(data.payload.d_w);
+                    // logger.info(data.payload.d_w);
                     const response = await fetch("http://supervisor/core/api/states/sensor.sense_realtime_energy_usage", {
                         method: 'POST',
                         body: JSON.stringify({
@@ -140,6 +176,10 @@ const connect = async function (conf) {
                     i = 0;
                 }
                 i++;
+            } else if (type === "monitor_info" || type === "data_change") {
+                logger.info(JSON.stringify(data, 0,0));
+            } else {
+                logger.info(JSON.stringify(data, 0,0));
             }
         });
     });
